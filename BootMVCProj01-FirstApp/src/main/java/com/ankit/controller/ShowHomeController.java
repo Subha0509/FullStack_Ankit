c8b22993e9bd499c262eb0efb9193524a66bfca1
saosu <saosu@Subhadeep>
2024-11-29 10:41:51 +0530

    Initial Commit

---
+++ BootMVCProj01-FirstApp/src/main/java/com/ankit/controller/ShowHomeController.java
@@ -0,0 +1,12 @@
+package com.ankit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ShowHomeController {
+	@RequestMapping("/home")
+	public String showHome() {
+		return "welcome";
+	}
+}
